afe09b052ac020255a333188b15d70865058b028
Murilo <mhscapim@gmail.com>
2020-06-03 18:03:51 -0300

    First commit

---
+++ src/stores/home.store.tsx
@@ -0,0 +1,33 @@
+import * as starWarsApi from '../apis/star-wars.api'
+
+import { action, observable } from 'mobx';
+
+import { Film } from '../interfaces/star-wars.interface';
+
+export default class HomeStore {
+
+  @observable films: Film[] = [];
+  @observable film: Film | any = {};
+
+  @action getFilms = async () => {
+    try {
+      const { data: films } = await starWarsApi.getFilms();
+      this.films = films;
+    } catch (error) {
+      this.films = [];
+    }
+  }
+
+  @action getFilmById = async (id: number) => {
+    try {
+      this.film = {};
+      const { data: film } = await starWarsApi.getFilmById(id);
+      this.film = film;
+    } catch (error) {
+      this.film = {};
+    }
+  }
+
+}
+const homeStore = new HomeStore();
+export { homeStore };
